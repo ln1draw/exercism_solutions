02ffa684a96633c73cd3b579011e8bf613939010
ellen wondra <ellenfromillinois@gmail.com>
2020-07-06 13:54:04 -0700

    etl working

---
+++ etl/etl.js
@@ -0,0 +1,12 @@
+// const old = { 1: ['A'] };
+//     const expected = { a: 1 };
+
+export const transform = (old_vals) => {
+  var new_dict = {};
+  for (var point_val in old_vals) {
+    for (var letter = 0; letter < old_vals[point_val].length; letter++) {
+      new_dict[old_vals[point_val][letter].toLowerCase()] = parseInt(point_val);
+    }
+  }
+  return new_dict;
+};
