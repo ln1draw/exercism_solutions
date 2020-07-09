c5b5dbc84a001f52267075f2fe7aa6aaa8c5c393
ellen wondra <ellenfromillinois@gmail.com>
2020-07-08 19:05:48 -0700

    scrabble score works

---
+++ scrabble-score/scrabble-score.js
@@ -0,0 +1,16 @@
+export const score = (input) => {
+  var valset = {
+    'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'l': 1, 'n': 1, 'r': 1, 's': 1, 't':1,
+    'd': 2, 'g': 2,
+    'b': 3, 'c': 3, 'm': 3, 'p': 3,
+    'f': 4, 'h': 4, 'v': 4, 'w': 4, 'y': 4,
+    'k': 5,
+    'j': 8, 'x': 8,
+    'q': 10, 'z': 10
+  }
+  var ret = 0;
+  for (var i = 0; i < input.length; i++) {
+    ret = ret + valset[input[i].toLowerCase()];
+  }
+  return ret;
+};
